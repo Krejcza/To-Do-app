acf347de515a51e187b9efa6d838e3ed76409a61
Krejcza <155578815+Krejcza@users.noreply.github.com>
2024-02-25 13:20:51 +0100

    Předělán Javascript do mojí lépe čitelné podoby

--- my-script.js
+++ my-script.js
@@ -1,36 +1,47 @@
-const inputBox = document.querySelector('#input-box')
+//proměnné//
+let inputBox = document.querySelector('#input-box')
+let buttonClick = document.querySelector('#clicking')
 const listContainer = document.querySelector('#list-container')
 
-function addTask(){
+//Po kliknutí na tlačítko přidat//
+buttonClick.addEventListener('click', function(){
    if(inputBox.value === ''){
-      alert('Musíte vyplnit pole!')
-   } else{
+      alert('Musíte něco vypsat!')
+   } else{ 
+
       let li = document.createElement('li')
       li.innerHTML = inputBox.value
       listContainer.appendChild(li)
+
       let span = document.createElement('span')
       span.innerHTML = '\u00d7'
       li.appendChild(span)
    }
+   
    inputBox.value = ''
    saveData()
-}
-
+})
+   
+//Po kliknutí na různé úkoly jako položky LI ze seznamu//
 listContainer.addEventListener('click', function(e){
    if(e.target.tagName === 'LI'){
       e.target.classList.toggle('checked')
       saveData()
-   } else if(e.target.tagName === 'SPAN')
+   } else if(e.target.tagName === 'SPAN'){
       e.target.parentElement.remove()
       saveData()
    }
-)
+})
 
+
+//Uloží dané hodnoty do místního úložiště webu s klíčem DATA//
 function saveData(){
    localStorage.setItem('data', listContainer.innerHTML)
 }
 
+//Funkce přidá uložená data z místního úložiště webu, aby se po znovuotevření načetla//
 function showTask(){
    listContainer.innerHTML = localStorage.getItem('data')
 }
+
 showTask()
